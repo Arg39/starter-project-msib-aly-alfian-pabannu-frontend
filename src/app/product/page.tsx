f54fcc8d700bd51cf0c958b6ca81c60ebd99c00b
Arg39 <pabannu@gmail.com>
2024-04-17 20:42:05 +0800

    first commit

---
+++ src/app/product/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+        <div>product 1</div>
+        <div>product 2</div>
+        <div>product 3</div>
+    </div>
+  )
+}
